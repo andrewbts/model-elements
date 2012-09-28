85f245a01848068e6134cc70ae3de5c3f496200e
Joel VanderWerf <vjoel@users.sourceforge.net>
2012-09-28 16:05:18 -0700

    Replace Time with DateTime and Duration; add joda conversion methods.

--- src/main/java/edu/berkeley/path/model_elements/Time.java
+++
@@ -1,4 +0,0 @@
-package edu.berkeley.path.model_elements;
-
-public class Time extends edu.berkeley.path.model_elements_base.Time {
-}
---
+++ src/test/java/edu/berkeley/path/model_elements/DateTimeTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2012 The Regents of the University of California.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+package edu.berkeley.path.model_elements;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DateTimeTest {
+  DateTime datetime;
+  org.joda.time.DateTime joda;
+
+  @Test
+  public void testJodaToCCToJoda() {
+    joda = new org.joda.time.DateTime(987654);
+    datetime = DateTime.fromJoda(joda);
+    
+    assertEquals(joda.getMillis(), datetime.toJoda().getMillis());
+  }
+  
+  @Test
+  public void testCCToJodaToCC() {
+    datetime = new DateTime();
+    datetime.milliseconds = 987654;
+    joda = datetime.toJoda();
+    
+    assertEquals(datetime.getMilliseconds(), DateTime.fromJoda(joda).getMilliseconds());
+  }
+  
+  
+
+}
