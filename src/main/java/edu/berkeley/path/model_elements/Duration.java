1b721983df775df079b4752a01d372bf23f6b257
Andrew Moylan <andrew@bt-systems.com>
2012-12-10 10:11:11 -0800

    Tidy, remove compiler warnings

--- src/main/java/edu/berkeley/path/model_elements/Duration.java
+++ src/main/java/edu/berkeley/path/model_elements/Duration.java
@@ -27,14 +27,16 @@
 package edu.berkeley.path.model_elements;
 
 public class Duration extends edu.berkeley.path.model_elements_base.Duration {
-  public static Duration fromJoda(org.joda.time.Duration joda) {
-    long ms = joda.getMillis();
-    Duration duration = new Duration();
-    duration.milliseconds = ms;
-    return duration;
-  }
-  
-  public org.joda.time.Duration toJoda() {
-    return new org.joda.time.Duration(milliseconds);
-  }
+
+	public static Duration fromJoda(org.joda.time.Duration joda) {
+		long ms = joda.getMillis();
+		Duration duration = new Duration();
+		duration.setMilliseconds(ms);
+		return duration;
+	}
+
+	public org.joda.time.Duration toJoda() {
+		return new org.joda.time.Duration(getMilliseconds());
+	}
+
 }
