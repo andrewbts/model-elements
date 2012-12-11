388c5683da9367990002bb31c08c2462b2628a8d
Andrew Moylan <andrew@bt-systems.com>
2012-12-11 14:38:54 -0800

    Tidy indentation

--- src/main/java/edu/berkeley/path/model_elements/Node.java
+++ src/main/java/edu/berkeley/path/model_elements/Node.java
@@ -29,87 +29,86 @@ package edu.berkeley.path.model_elements;
 import java.util.*;
 
 public class Node extends edu.berkeley.path.model_elements_base.Node {
-  protected HashMap<Node, HashSet<Link>> outLinksToNode;
-  protected HashMap<Node, HashSet<Link>> inLinksFromNode;
-  
-  public HashSet<Link> getAllOutLinks() {
-    HashSet<Link> links = new HashSet<Link>();
-    
-    for (Node node : this.outLinksToNode.keySet()) {
-      for (Link link : this.outLinksToNode.get(node)) {
-        links.add(link);
-      }
-    }
-    
-    return links;
-  }
-  
-  public HashSet<Link> getAllInLinks() {
-    HashSet<Link> links = new HashSet<Link>();
-    
-    for (Node node : this.inLinksFromNode.keySet()) {
-      for (Link link : this.inLinksFromNode.get(node)) {
-        links.add(link);
-      }
-    }
-    
-    return links;
-  }
-  
-  public HashSet<Link> getOutLinksToNode(Node node) {
-    return outLinksToNode.get(node);
-  }
+	protected HashMap<Node, HashSet<Link>> outLinksToNode;
+	protected HashMap<Node, HashSet<Link>> inLinksFromNode;
 
-  public HashSet<Link> getInLinksFromNode(Node node) {
-    return inLinksFromNode.get(node);
-  }
-  
-  public void resolveReferences(Link link) {
-    HashSet<Link> links;
-    
-    if (link.begin == this) {
-      if (this.outLinksToNode == null) {
-        this.outLinksToNode = new HashMap<Node, HashSet<Link>>();
-      }
-      
-      links = this.outLinksToNode.get(link.end);
-      if (links == null) {
-        links = new HashSet<Link>();
-        this.outLinksToNode.put(link.end, links);
-      }
-      links.add(link);
-    }
-    else if (link.end == this) {
-      if (this.inLinksFromNode == null) {
-        this.inLinksFromNode = new HashMap<Node, HashSet<Link>>();
-      }
-      
-      links = this.inLinksFromNode.get(link.begin);
-      if (links == null) {
-        links = new HashSet<Link>();
-        this.inLinksFromNode.put(link.begin, links);
-      }
-      links.add(link);
-    }
-  }
-  
-  public Long getLongId() {
-    return Long.parseLong(getId().toString());
-  }
-  
-  public void setId(Long id) {
-    setId(id.toString());
-  }
-  
-  public boolean isTerminal() {
-    return getType().equals("Terminal");
-  }
-  
-  public String getNameString() {
-    return (name == null) ? null : name.toString();
-  }
-  
-  public String getTypeString() {
-    return (type == null) ? null : type.toString();
-  }
+	public HashSet<Link> getAllOutLinks() {
+		HashSet<Link> links = new HashSet<Link>();
+
+		for (Node node : this.outLinksToNode.keySet()) {
+			for (Link link : this.outLinksToNode.get(node)) {
+				links.add(link);
+			}
+		}
+
+		return links;
+	}
+
+	public HashSet<Link> getAllInLinks() {
+		HashSet<Link> links = new HashSet<Link>();
+
+		for (Node node : this.inLinksFromNode.keySet()) {
+			for (Link link : this.inLinksFromNode.get(node)) {
+				links.add(link);
+			}
+		}
+
+		return links;
+	}
+
+	public HashSet<Link> getOutLinksToNode(Node node) {
+		return outLinksToNode.get(node);
+	}
+
+	public HashSet<Link> getInLinksFromNode(Node node) {
+		return inLinksFromNode.get(node);
+	}
+
+	public void resolveReferences(Link link) {
+		HashSet<Link> links;
+
+		if (link.begin == this) {
+			if (this.outLinksToNode == null) {
+				this.outLinksToNode = new HashMap<Node, HashSet<Link>>();
+			}
+
+			links = this.outLinksToNode.get(link.end);
+			if (links == null) {
+				links = new HashSet<Link>();
+				this.outLinksToNode.put(link.end, links);
+			}
+			links.add(link);
+		} else if (link.end == this) {
+			if (this.inLinksFromNode == null) {
+				this.inLinksFromNode = new HashMap<Node, HashSet<Link>>();
+			}
+
+			links = this.inLinksFromNode.get(link.begin);
+			if (links == null) {
+				links = new HashSet<Link>();
+				this.inLinksFromNode.put(link.begin, links);
+			}
+			links.add(link);
+		}
+	}
+
+	public Long getLongId() {
+		return Long.parseLong(getId().toString());
+	}
+
+	public void setId(Long id) {
+		setId(id.toString());
+	}
+
+	public boolean isTerminal() {
+		return getTypeString().equals("Terminal");
+	}
+
+	public String getNameString() {
+		return (name == null) ? null : name.toString();
+	}
+
+	public String getTypeString() {
+		return (type == null) ? null : type.toString();
+	}
 }
